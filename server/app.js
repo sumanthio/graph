a413f2cf2796c7a863eb78d7ace4d1ab5c6b8716
sumanthio <sumanth@bytemark.co>
2018-11-23 17:49:17 +0530

    GraphQL added and basic schema created

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
+const schema = require('./schemas/schema');
 const app = express();
 
+app.use('/graphql', graphqlHTTP({
+  schema
+}));
 
 app.listen(1024, () => {
   console.log('Listening on 1024');
---
+++ server/schemas/schema.js
@@ -0,0 +1,37 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+
+
+// book(id: '123'){
+//   name
+//   genre
+// }
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get date from DB
+      }
+    }
+  }
+});
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
