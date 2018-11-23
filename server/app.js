51d52ed2a0ab7051c333c625da145e66c0b36b4b
sumanthio <sumanth@bytemark.co>
2018-11-24 03:38:47 +0530

    Things to do

--- server/app.js
+++ server/app.js
@@ -3,6 +3,14 @@ const graphqlHTTP = require('express-graphql');
 const schema = require('./schemas/schema');
 const app = express();
 
+//Need to be added:
+// mongoose schema.
+// Create mlab collections
+// Mutations with the collections
+// GraphQLlists of the entire endpoints
+// Usecase of a DS with proper set of data
+
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
