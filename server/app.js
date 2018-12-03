fa32806ecfe359d34630c6dc07c329b6e220379e
sumanthio <sumanth@bytemark.co>
2018-12-03 19:17:40 +0530

    mlab collection created

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schemas/schema');
+const DB = require('./config');
 const app = express();
 
 // Need to be added:
+// Hook the data to the mlab Collection
 // mongoose schema.
-// Create mlab collections
 // Mutations with the collections
-// GraphQLlists of the entire endpoints
-// Usecase of a DS with proper set of data
+// Write mutations for adding house and character
 
 
 app.use('/graphql', graphqlHTTP({
