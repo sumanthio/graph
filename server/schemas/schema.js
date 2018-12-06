f216b27d2702227f5869e79dd1223137087a18e6
sumanthio <sumanth@bytemark.co>
2018-12-06 19:37:34 +0530

    DB hooked properly. Needs polish on all the requests

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -1,265 +1,7 @@
 import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
-import find from 'lodash/find';
 //Models
 import Character from '../models/character';
 import House from '../models/house';
-
-
-// const Houses = [
-//   {
-//     "id": "1",
-//     "name": "Stark",
-//     "characters": [
-//       "Arya Stark",
-//       "Benjen Stark",
-//       "Bran Stark",
-//       "Catelyn Stark",
-//       "Eddard Stark",
-//       "Ghost",
-//       "Grey Wind",
-//       "Jon Snow",
-//       "Lady",
-//       "Nymeria",
-//       "Rickon Stark",
-//       "Robb Stark",
-//       "Sansa Stark",
-//       "Shaggydog",
-//       "Summer"
-//     ]
-//   },
-//   {
-//     "name": "Targaryen",
-//     "characters": [
-//       "Daenerys Targaryen",
-//       "Drogon",
-//       "Rhaegal",
-//       "Viserion",
-//       "Viserys Targaryen"
-//     ]
-//   },
-//   {
-//     "name": "Baratheon",
-//     "characters": [
-//       "Joffrey Baratheon",
-//       "Myrcella Baratheon",
-//       "Renly Baratheon",
-//       "Robert Baratheon",
-//       "Selyse Baratheon",
-//       "Shireen Baratheon",
-//       "Stannis Baratheon",
-//       "Tommen Baratheon"
-//     ]
-//   },
-//   {
-//     "name": "Lannister",
-//     "characters": [
-//       "Cersei Lannister",
-//       "Jaime Lannister",
-//       "Kevan Lannister",
-//       "Lancel Lannister",
-//       "Tyrion Lannister",
-//       "Tywin Lannister"
-//     ]
-//   },
-//   {
-//     "name": "Night's Watch",
-//     "characters": [
-//       "Alliser Thorne",
-//       "Eddison Tollett",
-//       "Grenn",
-//       "Jeor Mormont",
-//       "Karl Tanner",
-//       "Maester Aemon",
-//       "Olly",
-//       "Othell Yarwyck",
-//       "Pypar",
-//       "Qhorin Halfhand",
-//       "Rast",
-//       "Samwell Tarly",
-//       "Yoren"
-//     ]
-//   },
-//   {
-//     "name": "Dothraki",
-//     "characters": [
-//       "Doreah",
-//       "Irri",
-//       "Khal Drogo",
-//       "Rakharo"
-//     ]
-//   },
-//   {
-//     "name": "Greyjoy",
-//     "characters": [
-//       "Balon Greyjoy",
-//       "Euron Greyjoy",
-//       "Theon Greyjoy",
-//       "Yara Greyjoy"
-//     ]
-//   },
-//   {
-//     "name": "Tyrell",
-//     "characters": [
-//       "Loras Tyrell",
-//       "Mace Tyrell",
-//       "Margaery Tyrell",
-//       "Olenna Tyrell"
-//     ]
-//   },
-//   {
-//     "name": "Wildling",
-//     "characters": [
-//       "Baby Sam",
-//       "Craster",
-//       "Gilly",
-//       "Mag the Mighty",
-//       "Mance Rayder",
-//       "Osha",
-//       "Rattleshirt",
-//       "Thenn Warg",
-//       "Tormund Giantsbane",
-//       "Wun Wun",
-//       "Ygritte"
-//     ]
-//   },
-//   {
-//     "name": "Martell",
-//     "characters": [
-//       "Doran Martell",
-//       "Ellaria Sand",
-//       "Nymeria Sand",
-//       "Obara Sand",
-//       "Oberyn Martell",
-//       "Trystane Martell",
-//       "Tyene Sand"
-//     ]
-//   },
-//   {
-//     "name": "Frey",
-//     "characters": [
-//       "Black Walder Rivers",
-//       "Lothar Frey",
-//       "Walder Frey"
-//     ]
-//   },
-//   {
-//     "name": "Tully",
-//     "characters": [
-//       "Brynden Tully",
-//       "Edmure Tully"
-//     ]
-//   },
-//   {
-//     "name": "White Walkers",
-//     "characters": [
-//       "Mag the Mighty Wight",
-//       "The Night King",
-//       "White Walker",
-//       "Wight Wildling Girl",
-//       "Wun Wun Wight"
-//     ]
-//   },
-//   {
-//     "name": "Include",
-//     "characters": [
-//       "Archmaester Marwyn",
-//       "Barristan Selmy",
-//       "Beric Dondarrion",
-//       "Brienne of Tarth",
-//       "Bronn",
-//       "Daario Naharis",
-//       "Davos Seaworth",
-//       "Gendry",
-//       "Grand Maester Pycelle",
-//       "Greatjon Umber",
-//       "Gregor Clegane",
-//       "Greizhen mo Ullhor",
-//       "Grey Worm",
-//       "Hallyne",
-//       "High Sparrow",
-//       "Hizdahr zo Loraq",
-//       "Hodor",
-//       "Hot Pie",
-//       "Janos Slynt",
-//       "Jaqen H'ghar",
-//       "Jojen Reed",
-//       "Jorah Mormont",
-//       "Jory Cassel",
-//       "Kraznys mo Nakloz",
-//       "Lady Crane",
-//       "Leaf",
-//       "Locke",
-//       "Lommy Greenhands",
-//       "Lord Varys",
-//       "Lyanna Mormont",
-//       "Lysa Arryn",
-//       "Maester Luwin",
-//       "Matthos Seaworth",
-//       "Meera Reed",
-//       "Melisandre",
-//       "Mirri Maz Duur",
-//       "Missandei",
-//       "Olyvar",
-//       "Petyr Baelish",
-//       "Podrick Payne",
-//       "Prendahl na Ghezn",
-//       "Pyat Pree",
-//       "Qyburn",
-//       "Ramsay Snow",
-//       "Razdal mo Eraz",
-//       "Rickard Karstark",
-//       "Robin Arryn",
-//       "Rodrik Cassel",
-//       "Roose Bolton",
-//       "Ros",
-//       "Salladhor Saan",
-//       "Sandor Clegane",
-//       "Septa Mordane",
-//       "Septa Unella",
-//       "Shae",
-//       "Silk King",
-//       "Smalljon Umber",
-//       "Syrio Forel",
-//       "Talisa Maegyr",
-//       "The Waif",
-//       "Thoros of Myr",
-//       "Three-Eyed Raven",
-//       "Xaro Xhoan Daxos",
-//       "Yezzan zo Qaggaz",
-//       "Yohn Royce"
-//     ]
-//   }
-// ]
-
-// const Characters = [
-//   {
-//     "id": "1",
-//     "characterName": "Arya Stark",
-//     "houseId": "1",
-//     "characterImageThumb": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk5MTYwNDc0OF5BMl5BanBnXkFtZTcwOTg2NDg1Nw@@._V1._SX100_SY140_.jpg",
-//     "characterImageFull": "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk5MTYwNDc0OF5BMl5BanBnXkFtZTcwOTg2NDg1Nw@@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-//     "actorName": "Maisie Williams",
-//     "siblings": [
-//       "Robb Stark",
-//       "Sansa Stark",
-//       "Bran Stark",
-//       "Rickon Stark"
-//     ],
-//     "kills": [
-//       "Red Keep Stableboy",
-//       "Frey Soldier #1",
-//       "Polliver",
-//       "Rorge",
-//       "Ghita",
-//       "Meryn Trant",
-//       "The Waif",
-//       "Black Walder Rivers",
-//       "Lothar Frey",
-//       "Walder Frey",
-//       "Petyr Baelish"
-//     ]
-//   }
-// ]
 
 const HouseType = new GraphQLObjectType({
   name: 'house',
@@ -283,8 +25,8 @@ const CharacterType = new GraphQLObjectType({
     kills: { type: new GraphQLList(GraphQLString) },
     house: {
       type: HouseType,
-      resolve(parent) {
-        return find(Houses, { id: parent.houseId })
+      resolve(parent, args) {
+        return House.findById(parent.id);
       }
     }
   })
@@ -297,15 +39,14 @@ const RootQuery = new GraphQLObjectType({
       type: HouseType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // code to get date from DB
-        return find(Houses, { id: args.id })
+        return House.findById(args.id);
       }
     },
     character: {
       type: CharacterType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return find(Characters, { id: args.id })
+        return Character.findById(args.id);
       }
     }
   }
